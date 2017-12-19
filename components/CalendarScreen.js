fa052bcfd898ef1e488c91345505c7eb9df94c0f
Warren-Partridge <maps@perdix.org>
2017-12-18 23:34:41 -0500

    Add mockup data for calendar and settings screens

--- components/CalendarScreen.js
+++ components/CalendarScreen.js
@@ -5,17 +5,39 @@ import { CalendarList } from 'react-native-calendars';
 
 class CalendarScreen extends React.Component {
     static navigationOptions = {
-        tabBarLabel: 'Calendar',
+        tabBarLabel: 'Progress',
     };
 
     render() {
+        const workout = {key:'workout', color: 'blue'};
         return(
             <View>
                 <CalendarList
                     scrollEnabled={true}
                     pastScrollRange={36}
                     futureScrollRange={36}
-                ></CalendarList>
+                    markedDates={{
+                        '2017-12-01': {selected: true},
+                        '2017-12-02': {selected: true},
+                        '2017-12-03': {disabled: true},
+                        '2017-12-04': {selected: true},
+                        '2017-12-08': {selected: true},
+                        '2017-12-12': {selected: true},
+                        '2017-12-14': {selected: true},
+                        '2017-12-15': {disabled: true},
+                        '2017-12-16': {selected: true},
+                        '2017-12-18': {selected: true},
+                        '2017-12-20': {dots: [workout]},
+                        '2017-12-22': {dots: [workout]},
+                        '2017-12-24': {dots: [workout]},
+                        '2017-12-27': {dots: [workout]},
+                        '2017-12-30': {dots: [workout]},
+                        '2017-12-31': {dots: [workout]},
+                        '2018-01-01': {dots: [workout]},
+                    }}
+                    markingType={'multi-dot'}
+                    onDayPress={(day) => {console.log(day)}}
+                />
             </View>
         );
     }
--- components/SettingsScreen.js
+++ components/SettingsScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, Image, Button, TouchableOpacity } from 'react-native';
 
 class SettingsScreen extends React.Component {
     static navigationOptions = {
@@ -9,8 +9,26 @@ class SettingsScreen extends React.Component {
 
     render() {
         return(
-            <View>
-                <Text>bonjour mon ami</Text>
+            <View style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <Text/>
+                <Text style={{fontWeight: 'bold', fontSize: 30}}>Your settings</Text>
+                <Text style={{fontSize: 15}}>What times per week will you work out? Remember, only your partner may change these settings from now on.</Text>
+                <Text style={{fontSize: 15}}/>
+                <TouchableOpacity onPress={() => {console.log("Self settings pressed")}} >
+                    <Image source={require('../resources/mocksegmentedcontrol.png')}/>
+                </TouchableOpacity>
+                <Text style={{fontSize: 60}}/>
+
+                <Text style={{fontSize: 85}}> </Text>
+                <Text style={{fontWeight: 'bold', fontSize: 30}}>Your partner's settings</Text>
+                <Text style={{fontSize: 15}}>What times per week will they work out?</Text>
+                <Text style={{fontSize: 15}}/>
+                <TouchableOpacity onPress={() => {console.log("Partner settings pressed")}} >
+                    <Image source={require('../resources/mocksegmentedcontrol.png')}/>
+                </TouchableOpacity>
             </View>
         );
     }
